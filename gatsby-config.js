1f2c7718adf5891bf3277ef0aa366e32be36e2e7
william <williamimoh@yahoo.com>
2021-03-21 14:18:59 +0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options:{
+        trackingIds: [
+          "G-Q2L40J8LBE"
+        ]
+      }
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
